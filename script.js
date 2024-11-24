3e95b4874959d1e5709fdb08a9bcda8b8cca21b2
alexandrertav <xandy.poa@hotmail.com>
2024-11-24 16:32:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função para exibir o formulário de inscrição
+function mostrarFormulario() {
+    document.getElementById("formulario-inscricao").style.display = "block"; // Torna o formulário visível
+}
+
+// Função para fechar o formulário
+function fecharFormulario() {
+    document.getElementById("formulario-inscricao").style.display = "none"; // Torna o formulário invisível
+}
